0fd8ba8b0b8e13561c0522b9e7461b35ee96c97f
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-09 09:34:47 +0200

    Register Controller 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,4 +7,13 @@ const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
 
-exports.register = async (req, res) => {}
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    const token = generateToken(user._id);
+    res.status(201).json({ userId: user._id, token });
+  } catch(err) {
+    res.status(400).json({ error: err.message });
+  };
+}
