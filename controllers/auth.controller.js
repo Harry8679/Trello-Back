c47f540acc6d5a2912550d1e7fc4b2334fb284ba
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-09 09:26:42 +0200

    generate Token

---
+++ controllers/auth.controller.js
@@ -0,0 +1,8 @@
+const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.require();
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
