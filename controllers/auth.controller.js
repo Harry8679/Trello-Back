0313c092581f5c46c8f6a02857ad0e8857a27328
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-09 09:27:19 +0200

    Register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,3 +6,5 @@ dotenv.require();
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
+
+exports.register = async (req, res) => {}
