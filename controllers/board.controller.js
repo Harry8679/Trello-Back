02654c7d9b22c7862ac5245d825b0497c34d5b17
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-13 23:02:17 +0200

    Create Board

--- controllers/board.controller.js
+++ controllers/board.controller.js
@@ -8,3 +8,13 @@ exports.getBoards = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+exports.createBoard = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const board = await Board.create({ title, userId: req.userId });
+    res.status(201).json(board);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
