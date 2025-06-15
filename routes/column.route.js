f8fcc936290fc18da29b8bbc6e918c780ee3d7c8
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-15 18:17:46 +0200

    Column Route

--- routes/column.route.js
+++ routes/column.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getBoards, createBoard } = require('../controllers/boardController');
+const { getBoards, createBoard } = require('../controllers/board.controller');
 const auth = require('../middlewares/auth.middleware');
 
 router.get('/', auth, getBoards);
