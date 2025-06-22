059611a39a61a16de27f7c999086cc8f34d2c40b
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-22 14:49:07 +0200

    Auth Controller 2

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/auth.controller');
+const { register, login, me } = require('../controllers/auth.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const User = require('../models/user.model');
 
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/me', authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findById(req.userId).select('-password');
-    if (!user) return res.status(404).json({ message: 'Utilisateur non trouvé' });
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: 'Erreur serveur' });
-  }
-});
+router.get('/me', authMiddleware, me);
 
 module.exports = router;
