fe69dfad042f20db70e05cd5bc7b9ff58fc3dcd6
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-24 17:18:45 +0200

    Update email route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, me } = require('../controllers/auth.controller');
+const { register, login, me, updateEmail } = require('../controllers/auth.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const User = require('../models/user.model');
 
@@ -8,5 +8,6 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me', authMiddleware, me);
+router.put('/update-email', authMiddleware, updateEmail);
 
 module.exports = router;
