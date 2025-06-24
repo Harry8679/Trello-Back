6d0f8656f5cbcbe150b9fdb5c2ffc939fe74ba88
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-24 22:16:25 +0200

    Update Avatar Url Route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, me, updateEmail, updatePassword, updateAvatarColor } = require('../controllers/auth.controller');
+const { register, login, me, updateEmail, updatePassword, updateAvatarColor, updateAvatarUrl } = require('../controllers/auth.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const User = require('../models/user.model');
 
@@ -12,5 +12,6 @@ router.put('/update-email', authMiddleware, updateEmail);
 router.put('/update-password', authMiddleware, updatePassword);
 
 router.put('/update-avatar-color', authMiddleware, updateAvatarColor);
+router.put('/update-avatar-url', auth, updateAvatarUrl);
 
 module.exports = router;
