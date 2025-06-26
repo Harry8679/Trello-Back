7c59bc6fae07100e48fb6f008a7858235c3b0ca1
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-26 10:56:40 +0200

    Card Route 2

--- routes/card.route.js
+++ routes/card.route.js
@@ -1,12 +1,11 @@
-// routes/cards.js
 const express = require('express');
 const { createCard, updateCard, reorderCard, deleteCard } = require('../controllers/card.controller');
-const auth = require('../middlewares/auth.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
 
-router.post('/columns/:columnId/cards', auth, createCard);
-router.put('/cards/:id', auth, updateCard);
-router.put('/cards/:id/order', auth, reorderCard);
-router.delete('/cards/:id', auth, deleteCard);
+router.post('/columns/:columnId/cards', authMiddleware, createCard);
+router.put('/cards/:id', authMiddleware, updateCard);
+router.put('/cards/:id/order', authMiddleware, reorderCard);
+router.delete('/cards/:id', authMiddleware, deleteCard);
 
 module.exports = router;
