f95493edf520ece46653199345934b402017fa1f
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-26 10:29:32 +0200

    Board Controller

--- controllers/board.controller.js
+++ controllers/board.controller.js
@@ -1,20 +1,38 @@
 const Board = require('../models/board.model');
-
-exports.getBoards = async (req, res) => {
-  try {
-    const boards = await Board.find({ userId: req.userId });
-    res.json(boards);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+const User = require('../models/user.model');
 
 exports.createBoard = async (req, res) => {
-  try {
-    const { title } = req.body;
-    const board = await Board.create({ title, userId: req.userId });
-    res.status(201).json(board);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
+  const { title } = req.body;
+  const ownerId = req.userId;
+  const board = await Board.create({
+    title,
+    owner: ownerId,
+    members: [{ user: ownerId, canCreateTasks: true }]
+  });
+  res.status(201).json(board);
+};
+
+exports.getBoards = async (req, res) => {
+  const userId = req.userId;
+  const boards = await Board.find({ 'members.user': userId });
+  res.json(boards);
+};
+
+exports.inviteToBoard = async (req, res) => {
+  const { id } = req.params;
+  const { userEmail, canCreateTasks = false } = req.body;
+  const user = await User.findOne({ email: userEmail });
+  if (!user) return res.status(404).json({ message: 'Utilisateur introuvable' });
+
+  const board = await Board.findById(id);
+  if (!board) return res.status(404).json({ message: 'Board introuvable' });
+
+  // Vérifier qu’on n’invite pas deux fois :
+  if (board.members.some(m => m.user.equals(user._id))) {
+    return res.status(400).json({ message: 'Déjà membre' });
   }
+
+  board.members.push({ user: user._id, canCreateTasks });
+  await board.save();
+  res.json(board);
 };
