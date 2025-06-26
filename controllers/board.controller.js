15fa9cfcdf6238467e587d3f6cc7d5c3b0499e5d
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-26 12:17:33 +0200

    Get Board

--- controllers/board.controller.js
+++ controllers/board.controller.js
@@ -46,3 +46,13 @@ exports.getBoardById = async (req, res) => {
     res.status(500).json({ message: 'Erreur serveur' });
   }
 };
+
+exports.getBoardById = async (req, res) => {
+  try {
+    const board = await Board.findById(req.params.id);
+    if (!board) return res.status(404).json({ message: 'Projet introuvable' });
+    res.json(board);
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+};
