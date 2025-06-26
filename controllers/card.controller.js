a6ba7615a41267707cc8d54c951c2aadccc880b8
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-26 10:36:57 +0200

    Card Controller

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -1,43 +1,39 @@
 const Card = require('../models/card.model');
 
-exports.getCards = async (req, res) => {
-  try {
-    const cards = await Card.find({ columnId: req.params.columnId }).sort('order');
-    res.json(cards);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
 exports.createCard = async (req, res) => {
-  try {
-    const { title, description, order } = req.body;
-    const card = await Card.create({ title, description, order, columnId: req.params.columnId });
-    res.status(201).json(card);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+  const { columnId } = req.params;
+  const { title, description, assignee } = req.body;
+  const card = await Card.create({
+    columnId,
+    title,
+    description,
+    assignee,
+    order: Date.now()
+  });
+  res.status(201).json(card);
 };
 
 exports.updateCard = async (req, res) => {
-  try {
-    const { title, description, order, columnId } = req.body;
-    const card = await Card.findByIdAndUpdate(
-      req.params.id,
-      { title, description, order, columnId },
-      { new: true }
-    );
-    res.json(card);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+  const { id } = req.params;
+  const updates = (({ title, description, assignee }) => ({ title, description, assignee }))(req.body);
+  const card = await Card.findByIdAndUpdate(id, updates, { new: true });
+  if (!card) return res.status(404).json({ message: 'Carte introuvable' });
+  res.json(card);
+};
+
+exports.reorderCard = async (req, res) => {
+  const { id } = req.params;
+  const { newOrder, newColumnId } = req.body;
+  const card = await Card.findById(id);
+  if (!card) return res.status(404).json({ message: 'Carte introuvable' });
+  card.order = newOrder;
+  if (newColumnId) card.columnId = newColumnId;
+  await card.save();
+  res.json(card);
 };
 
 exports.deleteCard = async (req, res) => {
-  try {
-    await Card.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Card deleted' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const { id } = req.params;
+  await Card.findByIdAndDelete(id);
+  res.sendStatus(204);
 };
