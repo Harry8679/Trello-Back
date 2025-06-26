dd14449c488e217c3cb1bb1456a3a03b4b869a87
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-26 10:03:18 +0200

    Board Model

--- models/board.model.js
+++ models/board.model.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+
 const boardSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  members: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    canCreateTasks: { type: Boolean, default: false }
+  }]
 });
+
 module.exports = mongoose.model('Board', boardSchema);
